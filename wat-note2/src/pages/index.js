312d7272824edd73b9268753e2e8e67b0704f573
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2022-05-11 10:13:50 +0900

    index.js　不要箇所削除

--- wat-note2/src/pages/index.js
+++ wat-note2/src/pages/index.js
@@ -7,7 +7,6 @@ import Seo from "../components/seo"
 
 const Index = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
-//  const posts = data.allMarkdownRemark.nodes
 
   return (
     <Layout location={location} title={siteTitle}>
@@ -29,44 +28,6 @@ const Index = ({ data, location }) => {
     </Layout>
   )
 
-  // return (
-  //   <Layout location={location} title={siteTitle}>
-  //     <Seo title="All posts" />
-  //     <Bio />
-  //     <ol style={{ listStyle: `none` }}>
-  //       {posts.map(post => {
-  //         const title = post.frontmatter.title || post.fields.slug
-
-  //         return (
-  //           <li key={post.fields.slug}>
-  //             <article
-  //               className="post-list-item"
-  //               itemScope
-  //               itemType="http://schema.org/Article"
-  //             >
-  //               <header>
-  //                 <h2>
-  //                   <Link to={post.fields.slug} itemProp="url">
-  //                     <span itemProp="headline">{title}</span>
-  //                   </Link>
-  //                 </h2>
-  //                 <small>{post.frontmatter.date}</small>
-  //               </header>
-  //               <section>
-  //                 <p
-  //                   dangerouslySetInnerHTML={{
-  //                     __html: post.frontmatter.description || post.excerpt,
-  //                   }}
-  //                   itemProp="description"
-  //                 />
-  //               </section>
-  //             </article>
-  //           </li>
-  //         )
-  //       })}
-  //     </ol>
-  //   </Layout>
-  // )
 }
 
 export default Index
