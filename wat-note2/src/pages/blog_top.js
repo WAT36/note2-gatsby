d91577d0351b56cde4546a0fddf260caad69ef76
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2022-05-13 00:29:29 +0900

    ブログのトップ画面の記事引っ張るGraphQLクエリを修正

--- wat-note2/src/pages/blog_top.js
+++ wat-note2/src/pages/blog_top.js
@@ -73,7 +73,7 @@ export const pageQuery = graphql`
         title
       }
     }
-    allMarkdownRemark(filter: {fileAbsolutePath: {regex: "/blog/"}}, sort: { fields: [frontmatter___date], order: DESC }) {
+    allMarkdownRemark(filter: {fileAbsolutePath: {regex: "//blog//"}}, sort: { fields: [frontmatter___date], order: DESC }) {
       nodes {
         excerpt
         fields {
