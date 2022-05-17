b4466fb256f31a47ca2fa31ab0dba1062050cde4
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2022-05-17 23:43:56 +0900

    レイアウトにtagを引数にしてタグバーに出す関数追加

--- wat-note2/src/components/layout.js
+++ wat-note2/src/components/layout.js
@@ -3,15 +3,24 @@ import * as React from "react"
 import Header from "../components/header"
 import ProgrammingLanguageTag from "./programming_language_tag"
 
-const Layout = ({ location, title, children }) => {
+const Layout = ({ location, title, children, plang_tags = [] }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
+
+  let tags=""
+  if (plang_tags.length > 0){
+      tags = plang_tags.map(tag => {
+        return
+          <ProgrammingLanguageTag name={tag}></ProgrammingLanguageTag>
+      })
+  }
+  //console.log("tags:"+tags);
 
   return (
     <div id="all-wrapper" >
       <Header title={title}></Header>
       <div className="programming-language-tagbar">
-                <ProgrammingLanguageTag name="test2"></ProgrammingLanguageTag>
+        {tags}
       </div>
       <div className="global-wrapper" data-is-root-path={isRootPath}>
         <main>{children}</main>
