6d7c45dc750ccc131635a05c661f7b5b00251b2a
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2022-05-23 12:11:36 +0900

    ディレクトリ名定義追加

--- wat-note2/src/common/constants.js
+++ wat-note2/src/common/constants.js
@@ -11,7 +11,13 @@ class Constants {
 
         //プログラミング以下
         "stdio"     :"標準入力・出力",
-        "string"    :"文字列"
+        "string"    :"文字列",
+        "number"    :"数値",
+
+        "log"       :"対数",
+        "round"     :"四捨五入",
+        "operator"  :"演算子",
+        "other_func":"その他関数"
     }
 
 
