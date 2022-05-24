60837d09fb403dd6ee282531e53e35b4a399cc21
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2022-05-24 11:09:11 +0900

    ディレクトリ名再設定

--- wat-note2/src/common/constants.js
+++ wat-note2/src/common/constants.js
@@ -13,11 +13,18 @@ class Constants {
         "stdio"     :"標準入力・出力",
         "string"    :"文字列",
         "number"    :"数値",
+        "list"      :"配列・リスト",
 
+        //// number以下
         "log"       :"対数",
         "round"     :"四捨五入",
         "operator"  :"演算子",
-        "other_func":"その他関数"
+        "other_func":"その他関数",
+        "base"      :"基数",
+
+        //// list以下
+        "array"     :"配列",
+        "list_tuple":"リスト（・タプル）"
     }
 
 
