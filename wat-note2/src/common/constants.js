c90626f8703c9cd5407f0a09b47da3c940c843f9
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2022-10-28 22:44:29 +0900

    フォルダ名少し変更

--- wat-note2/src/common/constants.js
+++ wat-note2/src/common/constants.js
@@ -1,70 +1,66 @@
 class Constants {
+  /**
+   * ディレクトリ名
+   */
+  static noteDirName = {
+    //各ディレクトリ
+    container: "コンテナ",
+    "front-end": "フロントエンド",
+    programming: "プログラミング",
 
-    /**
-     * ディレクトリ名
-     */
-    static noteDirName = {
-        //各ディレクトリ
-        "container" :"コンテナ",
-        "front-end" :"フロントエンド",
-        "programming":"プログラミング",
+    //プログラミング以下
+    "000_runtime": "実行環境",
+    "001_stdio": "標準入力・出力",
+    "002_string": "文字列",
+    "003_number": "数値",
+    "004_list": "配列・リスト",
+    "005_control": "制御構文",
+    "006_map": "辞書、Map、連想配列(オブジェクト)",
+    "007_class_func": "クラス・関数",
+    "008_exception": "例外処理",
+    "009_file_io": "ファイル入出力",
+    "010_other": "その他",
+    "011_graph_data": "グラフ・データ処理",
+    "012_machine_learning": "機械学習",
+    "013_procon": "競プロテク",
 
-        //プログラミング以下
-        "000_runtime"   :"実行環境",
-        "001_stdio"     :"標準入力・出力",
-        "002_string"    :"文字列",
-        "003_number"    :"数値",
-        "004_list"      :"配列・リスト",
-        "005_control"   :"制御構文",
-        "006_map"       :"辞書、Map",
-        "007_class_func":"クラス・関数",
-        "008_exception" :"例外処理",
-        "009_file_io"   :"ファイル入出力",
-        "010_other"     :"その他",
-        "011_graph_data":"グラフ・データ処理",
-        "012_machine_learning":"機械学習",
-        "013_procon"    :"競プロテク",
+    //// number以下
+    "001_log": "対数",
+    "002_round": "四捨五入",
+    "003_operator": "演算子",
+    "004_other_func": "その他関数",
+    "005_base": "基数",
 
-        //// number以下
-        "001_log"       :"対数",
-        "002_round"     :"四捨五入",
-        "003_operator"  :"演算子",
-        "004_other_func":"その他関数",
-        "005_base"      :"基数",
+    //// list以下
+    "001_array": "配列",
+    "002_list_tuple": "リスト（・タプル）",
 
-        //// list以下
-        "001_array"     :"配列",
-        "002_list_tuple":"リスト（・タプル）",
+    //// class_func以下
+    "001_class": "クラス",
+    "002_function": "関数・メソッド",
 
-        //// class_func以下
-        "001_class"     :"クラス",
-        "002_function"  :"関数・メソッド",
+    //// graph_data以下
+    "001_graph": "グラフ",
+    "101_2Dgraph": "２次元グラフ",
+    "102_3Dgraph": "３次元グラフ",
 
-        //// graph_data以下
-        "001_graph"     :"グラフ",
-        "101_2Dgraph"   :"２次元グラフ",
-        "102_3Dgraph"   :"３次元グラフ",
+    //// machine_learning以下
+    "001_supervised_learning": "教師あり学習",
+    "002_unsupervised_learning": "教師なし学習",
+    "003_deep_learning": "深層学習(ディープラーニング)",
+    "004_math": "数学系",
+    "101_regression": "回帰",
+    "102_classification": "分類",
 
-        //// machine_learning以下
-        "001_supervised_learning"   :"教師あり学習",
-        "002_unsupervised_learning" :"教師なし学習",
-        "003_deep_learning"         :"深層学習(ディープラーニング)",
-        "004_math"                  :"数学系",
-        "101_regression"            :"回帰",
-        "102_classification"        :"分類",
+    //// procon以下
+    "001_search": "探索",
+    "002_data_structure": "データ構造",
+    "003_graph": "グラフ",
 
-        //// procon以下
-        "001_search"            :"探索",
-        "002_data_structure"    :"データ構造",
-        "003_graph"             :"グラフ",
-
-        //// フロントエンド以下
-        "001_html"              :"HTML",
-        "002_CSS"               :"CSS",
-    }
-
-
-
+    //// フロントエンド以下
+    "001_html": "HTML",
+    "002_CSS": "CSS",
+  }
 }
 
-export default Constants;
+export default Constants
